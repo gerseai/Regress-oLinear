91ef4b5affe26621f8ff05b3117039392bf450e0
VictorPoldauf <vfpoldauf@gmail.com>
2019-05-19 14:10:58 -0300

    Organizando repositorio e subindo exemplo

--- Victor , material da aula/RegressaoLinearExecutavel.py
+++ Victor , material da aula/RegressaoLinearExecutavel.py
@@ -1,21 +1,22 @@
-import numpy as np
+# importando as bibiliotecas
+
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
-dados = pd.read_csv("Painted data.csv", delimiter=',')
+
+#Lendo documentos
+dados = pd.read_csv("Dados retirados do orange.csv", delimiter=',')
 dados.head()
 #Visualizando documentos
 dados.plot.scatter(x='x',y='y')
 # é necessário adicionar uma constante a matriz X
 xMaisConstantes = sm.add_constant(dados['x'])
-
 # OLS vem de Ordinary Least Squares e o método fit irá treinar o modelo
 results = sm.OLS(dados['y'], xMaisConstantes).fit()
-
 # mostrando as estatísticas do modelo
 results.summary()
 #Mostrando graficamente
-sns.regplot(x = 'x' , y = 'y' , data = dados, color ="b",  marker="p" ,  ci=80, x_bins=10)
-plt.show()
+fig = sns.regplot(x = 'x' , y = 'y' , data = dados, color ="r",  marker="o" ,  ci=100)
+plt.show(fig)
+#, x_bins=10
